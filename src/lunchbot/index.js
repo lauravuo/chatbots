d45789223e5dc345578b8e310954891da5edb514
Laura Vuorenoja <laura.vuorenoja@newthings.co>
2018-03-28 13:27:29 +0300

    Fix cronjob schedule. Fix menu parsing.

--- src/lunchbot/index.js
+++ src/lunchbot/index.js
@@ -4,15 +4,34 @@ import request from 'request';
 import logger from '../log';
 
 const menuUrl = 'http://pompier.fi/albertinkatu/lounas/';
+const weekdays = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai'];
 
 const parsePompierMenu = (html) => {
+
+  const dayIndex = new Date().getDay() - 1;
+  if (dayIndex < 0 || dayIndex >= weekdays.length) {
+    return 'Lunch available only weekdays!';
+  }
+
+  // Parse menu text
   const jsdom = new JSDOM(html);
-  const elementIndex = new Date().getDay() + 3;
-  const cssSelector = `.page-content :nth-child(${elementIndex})`;
-  // TODO: content validation
-  const linesString = jsdom.window.document.querySelector(cssSelector).textContent;
-  const lines = linesString.split('\n');
-  return [`<${menuUrl}|${lines[0]}>`, ...lines.slice(1, lines.length)].join('\n');
+  const cssSelector = '.page-content';
+  const menu = jsdom.window.document.querySelector(cssSelector).textContent;
+
+  // Find correct substring from menu
+  const startIndex = menu.indexOf(weekdays[dayIndex]);
+  const endIndex = dayIndex < weekdays.length - 1
+    ? menu.indexOf(weekdays[dayIndex + 1])
+    : menu.length;
+  if (startIndex >= 0 && startIndex < endIndex) {
+    const linesString = menu.substring(startIndex, endIndex);
+    const lines = linesString.split('\n');
+
+    // Format output
+    return [`<${menuUrl}|${lines[0]}>`, ...lines.slice(1, lines.length)].join('\n');
+  }
+
+  return 'Error fetching Pompier lunch information';
 };
 
 const fetchPompierMenu = async (url = menuUrl) => new Promise((resolve) => {
@@ -32,6 +51,7 @@ const fetchPompierMenu = async (url = menuUrl) => new Promise((resolve) => {
 (async () => {
   logger.info('Fetching Pompier menu for today...');
   const text = await fetchPompierMenu();
+  logger.info(`Result:\n${text}`);
   const payload = { text };
   // TODO: env variable validation
   request({
